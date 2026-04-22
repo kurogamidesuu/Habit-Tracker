75bf487595f89396a52d45e2fa65de68e80aa1d9
kurogamidesuu <hempushpchauhan@gmail.com>
2026-04-22 22:18:06 +0530

    initial project scaffold (backend)

---
+++ README.md
@@ -0,0 +1,8 @@
+# Habit Tracker App
+
+A personal habit tracker to keep track of your habits. You can maintain streaks and look over your progress as you grow it.
+
+## Tech Stack
+I am using PERN (PostgreSQL, Express, React, Node.js)
+
+`Currently under development...`
---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is healthy and running!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
