09915c159b163ab77802e9cc07ca3e1edc2ca2dd
kurogamidesuu <hempushpchauhan@gmail.com>
2026-04-23 13:34:00 +0530

    switch to TS and setup Prisma ORM

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,17 @@
-import express from "express";
+import express, { Application, Request, Response,  } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/health", (req, res) => {
+app.get("/api/health", (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     message: "Server is healthy and running!",
