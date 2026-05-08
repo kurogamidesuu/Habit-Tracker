680dc8e7cef9b1b264a2bd9281456d41566ef195
kurogamidesuu <hempushpchauhan@gmail.com>
2026-05-09 02:26:47 +0530

    Refactor Login and Signup forms to use react-hook-form and add visual feedback

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,16 +1,20 @@
 import { Preferences } from "@capacitor/preferences";
-import { useState, type SubmitEvent } from "react";
+import { useForm } from "react-hook-form";
 import { Link, useNavigate, } from "react-router-dom";
+import { toast } from "react-toastify";
+
+interface LoginFormSchema {
+  email: string;
+  password: string;
+}
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const navigate = useNavigate();
+  const { handleSubmit, register, formState: { errors, isSubmitting } } = useForm<LoginFormSchema>();
 
-  const handleSubmit = async (e: SubmitEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    
+  const onSubmit = async (formData: LoginFormSchema) => {
+    const { email, password } = formData;
+
     try {
       const res = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/user/login`, {
         method: 'POST',
@@ -29,10 +33,12 @@ const LoginPage = () => {
         });
         navigate('/');
       } else {
-        console.log(data.message);
+        console.error(data.message);
+        toast.error(data.message);
       }
     } catch(e) {
       console.error('Something went wrong!', e);
+      toast.error('Something went wrong!');
     }
   }
 
@@ -43,35 +49,44 @@ const LoginPage = () => {
         <h1 className="text-[4em] font-bold italic -mt-5 text-transparent bg-gradient-to-r from-rose-700 to-purple-600 bg-clip-text">Kintsugi</h1>
       </div>
       <div className="w-full h-[67%] bg-gradient-to-br from-sky-600 via-sky-600 to-cyan-600 rounded-t-[20px]">
-        <form onSubmit={handleSubmit} className="w-full h-[80%] px-5 flex flex-col items-center justify-center gap-20">
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full h-[80%] px-5 flex flex-col items-center justify-center gap-20">
           <div className="grid grid-cols-10 items-center gap-5">
             {/* email */}
             <label htmlFor="email" className="text-lg font-semibold text-sky-100 col-span-3">Email:</label>
             <input
               id="email"
               type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              {...register("email", { required: "Email is required" })}
               className="h-10 bg-slate-200 rounded-sm col-span-7 px-2"
-              required
             />
+            {errors.email && (
+              <>
+                <div className="col-span-3" />
+                <p className="col-span-7 -mt-3 text-red-600 font-bold">{errors.email.message}</p>
+              </>
+            )}
 
             {/* password */}
             <label htmlFor="password" className="text-lg font-semibold text-sky-100 col-span-3">Password:</label>
             <input
               id="password"
               type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              {...register("password", { required: "Please enter a password" })}
               className="h-10 bg-slate-200 rounded-sm col-span-7 px-2"
-              required
             />
+            {errors.password && (
+              <>
+                <div className="col-span-3" />
+                <p className="col-span-7 -mt-3 text-red-600 font-bold">{errors.password.message}</p>
+              </>
+            )}
           </div>
 
 
           <div className="flex flex-col gap-5 items-center">
             <button
               type="submit"
+              disabled={isSubmitting}
               className="w-50 h-10 bg-gradient-to-br from-amber-400 via-amber-300 to-amber-500 rounded-[10px]">
               Login
             </button>
--- frontend/src/components/SignupPage.tsx
+++ frontend/src/components/SignupPage.tsx
@@ -1,17 +1,21 @@
 import { Preferences } from "@capacitor/preferences";
-import { useState, type SubmitEvent } from "react";
+import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+interface SignupFormSchema {
+  username: string;
+  email: string;
+  password: string;
+}
 
 const SignupPage = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const navigate = useNavigate();
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<SignupFormSchema>();
 
-  const handleSubmit = async (e: SubmitEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    
+  const onSubmit = async (formData: SignupFormSchema) => {
+    const { username, email, password } = formData;
+
     try {
       const res = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/user/signup`, {
         method: 'POST',
@@ -30,10 +34,12 @@ const SignupPage = () => {
         });
         navigate('/');
       } else {
-        console.log(data.message);
+        console.error(data.message);
+        toast.error(data.message);
       }
     } catch(e) {
       console.error('Something went wrong!', e);
+      toast.error('Something went wrong!');
     }
   }
 
@@ -44,45 +50,58 @@ const SignupPage = () => {
         <h1 className="text-[4em] font-bold italic -mt-5 text-transparent bg-gradient-to-r from-rose-700 to-purple-600 bg-clip-text">Kintsugi</h1>
       </div>
       <div className="w-full h-[67%] bg-gradient-to-br from-sky-600 via-sky-600 to-cyan-600 rounded-t-[20px]">
-        <form onSubmit={handleSubmit} className="w-full h-[80%] px-5 flex flex-col items-center justify-center gap-10">
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full h-[80%] px-5 flex flex-col items-center justify-center gap-10">
           <div className="grid grid-cols-10 items-center gap-5">
             {/* username */}
             <label htmlFor="username" className="text-lg font-semibold text-sky-100 col-span-3">Username:</label>
             <input
               id="username"
               type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="h-10 bg-slate-200 rounded-sm col-span-7"
-              required
+              {...register("username", { required: 'Please enter a valid username' })}
+              className="h-10 bg-slate-200 rounded-sm col-span-7 px-2"
             />
+            {errors.username && (
+              <>
+                <div className="col-span-3" />
+                <p className="col-span-7 -mt-3 text-red-600 font-bold">{errors.username.message}</p>
+              </>
+            )}
 
             {/* email */}
             <label htmlFor="email" className="text-lg font-semibold text-sky-100 col-span-3">Email:</label>
             <input
               id="email"
               type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="h-10 bg-slate-200 rounded-sm col-span-7"
-              required
+              {...register("email", { required: 'Email is required' })}
+              className="h-10 bg-slate-200 rounded-sm col-span-7 px-2"
             />
+            {errors.email && (
+              <>
+                <div className="col-span-3" />
+                <p className="col-span-7 -mt-3 text-red-600 font-bold">{errors.email.message}</p>
+              </>
+            )}
 
             {/* password */}
             <label htmlFor="password" className="text-lg font-semibold text-sky-100 col-span-3">Password:</label>
             <input
               id="password"
               type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="h-10 bg-slate-200 rounded-sm col-span-7"
-              required
+              {...register("password", { required: 'Please enter a password' })}
+              className="h-10 bg-slate-200 rounded-sm col-span-7 px-2"
             />
+            {errors.password && (
+              <>
+                <div className="col-span-3" />
+                <p className="col-span-7 -mt-3 text-red-600 font-bold">{errors.password.message}</p>
+              </>
+            )}
           </div>
 
           <div className="flex flex-col gap-5 items-center">
             <button
               type="submit"
+              disabled={isSubmitting}
               className="w-50 h-10 bg-gradient-to-br from-amber-400 via-amber-300 to-amber-500 rounded-[10px]">
               Signup
             </button>
