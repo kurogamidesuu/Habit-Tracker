24da3df6d89795f83c5c091689b6198f8721b987
kurogamidesuu <hempushpchauhan@gmail.com>
2026-05-17 13:29:12 +0530

    add https:localhost to CORS origin list

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 const app: Application = express();
 
 app.use(cors({
-  origin: ['capacitor://localhost', 'http://localhost', 'http://localhost:5173'],
+  origin: ['capacitor://localhost', 'http://localhost', 'https://localhost', 'http://localhost:5173'],
   credentials: true,
 }));
 app.use(express.json());
