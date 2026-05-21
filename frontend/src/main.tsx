81bb1998125a8a4a016e5c436684d08e3e054f38
kurogamidesuu <hempushpchauhan@gmail.com>
2026-05-21 16:17:33 +0530

    fix stale time for cache

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,7 +12,7 @@ const persister = experimental_createQueryPersister({
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 0,
+      staleTime: 1000 * 30,
       gcTime: 1000 * 60 * 60 * 24,
       retry: 2,
       refetchOnWindowFocus: true,
