6288466ab8fc5f3df94d7b58f7328a43b3723434
kurogamidesuu <hempushpchauhan@gmail.com>
2026-05-31 14:04:14 +0530

    Fix CORS issues in backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ const app: Application = express();
 
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [ process.env.FRONTEND_URL!, process.env.BACKEND_BASE_URL! ],
   credentials: true,
 }));
 app.use(express.json());
